3b8dc8e32747e1fe643a35b4993f19cd23fd71a0
rriffa <145914122+rriffa@users.noreply.github.com>
2023-10-26 12:36:46 +0700

    Add files via upload

---
+++ src/latihan25/Latihan25.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan25;
+import java.util.Scanner;
+/**import java.util.Scanner;
+ *
+ * @author Lenovo
+ */
+public class Latihan25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        boolean ulangi = true;
+        while(ulangi){
+            System.out.print("Masukkan nama depan anda untuk dieja : ");
+            String nama = input.next();
+            
+            System.out.println("Ejaan untuk '"+nama+"' adalah ");
+            
+            int panjangNama = nama.length();
+            for(int i = 0; i<panjangNama; i++){
+                char huruf = nama.charAt(i);
+                System.out.println("Huruf ke-"+(i+1)+" : "+huruf);
+            }
+            
+            System.out.print("Ulangi Aktifitas? (ya/tidak)");
+            String ulang = input.next();
+            if(ulang == "ya"){
+                System.out.println("Program akan diulangi");
+            }else if(ulang == "tidak"){
+                System.out.println("Program berhenti terimakasih");
+                ulangi = false; 
+            }
+        }
+    }
+    
+}
